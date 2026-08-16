526342b9f204d425d9747d7fec04021751117027
NexusInfra123 <infra@nexustalentlabs.com>
2026-08-16 18:29:08 +0530

    push

--- sentry.edge.config.ts
+++ sentry.edge.config.ts
@@ -2,8 +2,6 @@ import * as Sentry from "@sentry/nextjs";
 
 Sentry.init({
   dsn: process.env.NEXT_PUBLIC_SENTRY_DSN || "https://examplePublicKey@o0.ingest.sentry.io/0",
-  org: "nexus-talent-labs",
-  project: "javascript-nextjs",
 
   // Adjust this value in production, or use tracesSampler for greater control
   tracesSampleRate: 1.0,
